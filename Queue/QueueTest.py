c261749eb6d401947cbf1cf33a1dfd34364d3b1f
Tymek399 <92734250+Tymek399@users.noreply.github.com>
2024-01-19 09:54:53 +0100

    QueueTest.py
    Test Algorytmu Queue

--- Queue/QueueTest.py
+++ Queue/QueueTest.py
@@ -1,1 +1,46 @@
+import unittest
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def add(self, item):
+        self.queue.append(item)
+
+    def remove(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            raise IndexError("Cannot remove from an empty queue")
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+class TestQueue(unittest.TestCase):
+    def test_queue_is_class(self):
+        self.assertTrue(callable(getattr(Queue, '__init__')))
+
+    def test_add_elements_to_queue(self):
+        q = Queue()
+        self.assertIsNotNone(q)
+        q.add(1)
+        self.assertEqual(q.queue, [1])
+
+    def test_remove_elements_from_queue(self):
+        q = Queue()
+        q.add(1)
+        self.assertEqual(q.remove(), 1)
+
+    def test_order_of_elements_is_maintained(self):
+        q = Queue()
+        q.add(1)
+        q.add(2)
+        q.add(3)
+        self.assertEqual(q.remove(), 1)
+        self.assertEqual(q.remove(), 2)
+        self.assertEqual(q.remove(), 3)
+        self.assertIsNone(q.remove())
+
+if __name__ == '__main__':
+    unittest.main()
 
