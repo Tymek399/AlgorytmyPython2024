35b6c138bc8916d316f484c72d1a664297799489
Tymek399 <92734250+Tymek399@users.noreply.github.com>
2024-01-19 10:57:23 +0100

    LevelWidth.py

--- LevelWidth/LevelWidth.py
+++ LevelWidth/LevelWidth.py
@@ -1,1 +1,25 @@
+ from collections import deque
+
+class Wezel:
+    def __init__(self, dane):
+        self.dane = dane
+        self.dzieci = []
+
+def szerokosc_poziomow(korzen):
+    if not korzen:
+        return []
+
+    wynik = []
+    kolejka = deque([korzen])
+
+    while kolejka:
+        rozmiar_poziomu = len(kolejka)
+        wynik.append(rozmiar_poziomu)
+
+
+        for _ in range(rozmiar_poziomu):
+            biezacy = kolejka.popleft()
+            kolejka.extend(biezacy.dzieci)
+
+    return wynik
 
